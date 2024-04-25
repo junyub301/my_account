d00f8e16eed966ccfbbe5e41412f7306e6e33d31
junyub301 <junyub0301@gmail.com>
2024-04-25 22:50:09 +0900

    프로젝트 환결 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Layout from '@components/shared/Layout'
+import { Global } from '@emotion/react'
+import globalStyles from '@styles/globalStyles'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
