ba60a0693b2aa9b435fb389482877a030f2eb384
junyub301 <junyub0301@gmail.com>
2024-04-28 18:38:27 +0900

    Feat: 신용점수 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import Account from '@components/home/Account'
+import { CreditScoreSkeleton } from '@components/home/CreditScore'
 import { BannerSkeleton } from '@components/home/EventBanners'
 import Skelton from '@shared/Skeleton'
+import Spacing from '@shared/Spacing'
 import dynamic from 'next/dynamic'
 
 const EventBanners = dynamic(() => import('@components/home/EventBanners'), {
   ssr: false,
   loading: () => <BannerSkeleton />,
+})
+const CreditScore = dynamic(() => import('@components/home/CreditScore'), {
+  ssr: false,
+  loading: () => <CreditScoreSkeleton />,
 })
 
 export default function Home() {
@@ -13,6 +19,8 @@ export default function Home() {
     <>
       <EventBanners />
       <Account />
+      <Spacing size={8} backgroundColor="gray100" />
+      <CreditScore />
     </>
   )
 }
