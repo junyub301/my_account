fc460d969632c5ffdc97f177f5fa58e32d091f88
junyub301 <junyub0301@gmail.com>
2024-04-28 19:01:26 +0900

    Feat: 카드 리스트 추가

---
+++ src/components/home/hooks/useCards.ts
@@ -0,0 +1,6 @@
+import { getCards } from '@remote/card'
+import { useQuery } from 'react-query'
+
+export default function useCards() {
+  return useQuery(['cards'], () => getCards(), { suspense: true })
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Account from '@components/home/Account'
+import { CardListSkeleton } from '@components/home/CardList'
 import { CreditScoreSkeleton } from '@components/home/CreditScore'
 import { BannerSkeleton } from '@components/home/EventBanners'
-import Skelton from '@shared/Skeleton'
 import Spacing from '@shared/Spacing'
 import dynamic from 'next/dynamic'
 
@@ -13,6 +13,10 @@ const CreditScore = dynamic(() => import('@components/home/CreditScore'), {
   ssr: false,
   loading: () => <CreditScoreSkeleton />,
 })
+const CardList = dynamic(() => import('@components/home/CardList'), {
+  ssr: false,
+  loading: () => <CardListSkeleton />,
+})
 
 export default function Home() {
   return (
@@ -21,6 +25,8 @@ export default function Home() {
       <Account />
       <Spacing size={8} backgroundColor="gray100" />
       <CreditScore />
+      <Spacing size={8} backgroundColor="gray100" />
+      <CardList />
     </>
   )
 }
