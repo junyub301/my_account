b6143b0923978ef933b38789d37c82be94526ede
junyub301 <junyub0301@gmail.com>
2024-05-23 21:22:36 +0900

    Feat:저금통 정보

--- src/components/account/PiggyBankRow.tsx
+++ src/components/account/PiggyBankRow.tsx
@@ -1,10 +1,59 @@
+import useUser from '@hooks/useUser'
+import withSuspense from '@hooks/withSuspense'
+import { getPiggyBank } from '@remote/piggybank'
+import Flex from '@shared/Flex'
 import ListRow from '@shared/ListRow'
+import Text from '@shared/Text'
+import addDelimiter from '@utils/addDelimiter'
+import { differenceInDays } from 'date-fns'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import React from 'react'
+import { useQuery } from 'react-query'
 
-export default function PiggyBankRow() {
+function PiggyBankRow() {
   const navigate = useRouter()
+  const user = useUser()
+  const { data } = useQuery(
+    ['piggy_bank', user?.id],
+    () => getPiggyBank(user?.id as string),
+    {
+      suspense: true,
+    },
+  )
+
+  if (data == null) {
+    return (
+      <div>
+        <ul>
+          <ListRow
+            left={
+              <Image
+                src="https://cdn4.iconfinder.com/data/icons/PLASTICXP/accounting/png/64/deposit.png"
+                width={40}
+                height={40}
+                alt=""
+              />
+            }
+            contents={
+              <ListRow.Texts
+                title="저금통"
+                subTitle="매일 매일 조금씩 저금하여 목표금액을 모아보아요"
+              />
+            }
+            withArrow
+            onClick={() => {
+              navigate.push('/account/piggybank/new')
+            }}
+          />
+        </ul>
+      </div>
+    )
+  }
+
+  const { balance, endDate, goalAmount } = data
+  const dDay = differenceInDays(endDate, new Date())
+
   return (
     <div>
       <ul>
@@ -18,17 +67,19 @@ export default function PiggyBankRow() {
             />
           }
           contents={
-            <ListRow.Texts
-              title="저금통"
-              subTitle="매일 매일 조금씩 저금하여 목표금액을 모아보아요"
-            />
+            <Flex direction="column">
+              <Text typography="t4" bold>
+                D-{dDay}
+              </Text>
+              <Text>{addDelimiter(goalAmount - balance)}원 남았어요</Text>
+            </Flex>
           }
           withArrow
-          onClick={() => {
-            navigate.push('/account/piggybank/new')
-          }}
+          onClick={() => {}}
         />
       </ul>
     </div>
   )
 }
+
+export default withSuspense(PiggyBankRow, { fallback: <div>로딩중 ...</div> })
--- src/remote/piggybank.ts
+++ src/remote/piggybank.ts
@@ -1,8 +1,41 @@
 import { COLLECTIONS } from '@constants/collection'
 import { PiggyBank } from '@models/piggybank'
-import { collection, doc, setDoc } from 'firebase/firestore'
+import {
+  collection,
+  doc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  setDoc,
+  where,
+} from 'firebase/firestore'
 import { store } from './firebase'
 
 export function createPiggyBank(newPiggyBank: PiggyBank) {
   return setDoc(doc(collection(store, COLLECTIONS.PIGGYBANK)), newPiggyBank)
+}
+
+export async function getPiggyBank(userId: string) {
+  const snapshot = await getDocs(
+    query(
+      collection(store, COLLECTIONS.PIGGYBANK),
+      where('userId', '==', userId),
+      where('endDate', '>=', new Date()),
+      orderBy('endDate', 'asc'),
+      limit(1),
+    ),
+  )
+
+  if (snapshot.docs.length === 0) {
+    return null
+  }
+
+  const piggyBank = snapshot.docs[0].data()
+  return {
+    id: snapshot.docs[0].id,
+    ...(piggyBank as PiggyBank),
+    startDate: piggyBank.startDate.toDate(),
+    endDate: piggyBank.endDate.toDate(),
+  }
 }
