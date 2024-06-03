6415ac3bcd3948f309353385a66661ea44839a13
junyub301 <junyub0301@gmail.com>
2024-06-03 20:57:59 +0900

    Feat: webvital추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,13 +8,16 @@ import AuthGuard from '@components/auth/AuthGuard'
 import Navbar from '@shared/Navbar'
 import { AlertContextProvider } from '@contexts/AlertContext'
 import ErrorBoundary from '@shared/ErrorBoundary'
-
+import { useReportWebVitals } from 'next/web-vitals'
 const client = new QueryClient()
 
 export default function App({
   Component,
   pageProps: { dehydrateState, session, ...pageProps },
 }: AppProps) {
+  useReportWebVitals((metric) => {
+    console.log(metric)
+  })
   return (
     <Layout>
       <Global styles={globalStyles} />
